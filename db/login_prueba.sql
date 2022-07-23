ac34bec41c1063233eaeee1c16ee2f539a7cc0d1
Fredy Velandia <fredyve@gmail.com>
2022-07-22 20:54:04 -0500

    primera version

---
+++ db/login_prueba.sql
@@ -0,0 +1,19 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+DROP TABLE IF EXISTS `encuesta`;
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `correo` varchar(123) NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `clave` varchar(200) NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MEMORY AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+
+
